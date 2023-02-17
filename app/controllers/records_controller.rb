3e69afd4acc7eed4c670a5cfcfa713c1c204d8b2
sfsf700 <hirokazu.2rnk@gmail.com>
2023-02-17 14:58:56 +0900

    一覧表示の内容が月毎に表示されるように再修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -19,19 +19,28 @@ class RecordsController < ApplicationController
   end
 
   def index
+    
+    now_day = Date.today
+    sta = now_day.beginning_of_month
+    end_day = now_day.end_of_month
 
-    date = params[:start_date]
-    
-    s = date.to_date
-    sta_day = s.beginning_of_month
-    end_day = s.end_of_month
-    
-    @items = Record.all.where(user_id: current_user.id).group(:r_date).sum(:price)
-    @records = Record.includes(:user).order('r_date DESC').where(user_id: current_user.id).where(r_date: sta_day..end_day)
-    
-    @select_income = Record.where(status: 2).where(user_id: current_user.id).where(r_date: sta_day..end_day).sum(:price) 
-    @select_spending = Record.where(status: 1).where(user_id: current_user.id).where(r_date: sta_day..end_day).sum(:price) ## 支出合計
-    
+    simplecalendar = params[:start_date]
+
+    if simplecalendar != nil
+
+      select_beg_day = simplecalendar.to_date.beginning_of_month
+      select_end_day = simplecalendar.to_date.end_of_month
+
+      @records = Record.includes(:user).order('r_date DESC').where(user_id: current_user.id).where(r_date: select_beg_day..select_end_day)
+      @select_income = Record.where(status: 2).where(user_id: current_user.id).where(r_date: select_beg_day..select_end_day).sum(:price) ## 収入
+      @select_spending = Record.where(status: 1).where(user_id: current_user.id).where(r_date: select_beg_day..select_end_day).sum(:price) ## 支出
+    else
+      @records = Record.includes(:user).order('r_date DESC').where(user_id: current_user.id).where(r_date: sta..end_day)
+      @select_income = Record.where(status: 2).where(user_id: current_user.id).sum(:price) ## 収入
+      @select_spending = Record.where(status: 1).where(user_id: current_user.id).sum(:price) ## 支出
+    end
+
+    @items = Record.all.where(user_id: current_user.id).group(:r_date).sum(:price)    
   end
 
   private 
