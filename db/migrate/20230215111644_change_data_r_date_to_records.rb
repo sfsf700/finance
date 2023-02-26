5a089379a8fb326a1603ad79b21c3550679bb2db
sfsf700 <hirokazu.2rnk@gmail.com>
2023-02-26 20:43:49 +0900

    table修正3

--- db/migrate/20230215111644_change_data_r_date_to_records.rb
+++ db/migrate/20230215111644_change_data_r_date_to_records.rb
@@ -1,13 +1,12 @@
 class ChangeDataRDateToRecords < ActiveRecord::Migration[6.0]
+
   def up
-    
-    if Rails.env.development? || Rails.env.test?
+    if Rails.env.development?
       change_column :records, :r_date, :date
     else Rails.env.production?
-      # 本番環境はusingオプションを追加
-      change_column :records, :r_date, 'date USING CAST(r_date AS date)'
+      change_column :records, :r_date,  'date USING CAST(r_date AS date)'
     end
-  end
+  end 
 
   def down
     change_column :records, :r_date, :date
--- db/migrate/20230215151656_change_data_s_date_to_spendings.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataSDateToSpendings < ActiveRecord::Migration[6.0]
-  def change
-    change_column :spendings, :s_date, :date
-  end
-end
