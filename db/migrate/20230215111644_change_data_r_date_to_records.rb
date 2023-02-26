fc085fe7038aad4bd8eab517eb99c8cd4f091412
sfsf700 <hirokazu.2rnk@gmail.com>
2023-02-26 21:04:41 +0900

    再々修正

--- db/migrate/20230215111644_change_data_r_date_to_records.rb
+++ db/migrate/20230215111644_change_data_r_date_to_records.rb
@@ -1,14 +1,6 @@
 class ChangeDataRDateToRecords < ActiveRecord::Migration[6.0]
 
-  def up
-    if Rails.env.development?
-      change_column :records, :r_date, :date
-    else Rails.env.production?
-      change_column :records, :r_date,  'date USING CAST(r_date AS date)'
-    end
-  end 
-
-  def down
+  def chnage
     change_column :records, :r_date, :date
   end
 end
---
+++ db/migrate/20230226120214_change_data.rb
@@ -0,0 +1,13 @@
+class ChangeData < ActiveRecord::Migration[6.0]
+  def up
+    if Rails.env.development?
+      change_column :records, :r_date, :date
+    else Rails.env.production?
+      change_column :records, :r_date,  'date USING CAST(r_date AS date)'
+    end
+  end 
+
+  def down
+    change_column :records, :r_data, :data
+  end
+end
