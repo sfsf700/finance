79708102f2a1efed82b0f18d33d483485d4235ce
sfsf700 <hirokazu.2rnk@gmail.com>
2023-02-26 17:42:16 +0900

    table修正

--- db/migrate/20230215111644_change_data_r_date_to_records.rb
+++ db/migrate/20230215111644_change_data_r_date_to_records.rb
@@ -1,6 +1,6 @@
 class ChangeDataRDateToRecords < ActiveRecord::Migration[6.0]
   def change
-    change_column :records, :r_date, 'date USING CAST(r_date AS idate)' 
+    change_column :records, :r_date, 'date USING CAST(r_date AS date)' 
     
     
   end
